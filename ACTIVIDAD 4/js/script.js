d2d31bfe3f3582da50f7a9f5b087afef69c9bdc0
Marco Martínez Martínez <mmarmar1703@g.educaand.es>
2025-01-24 11:54:38 +0100

    Add files via upload

---
+++ ACTIVIDAD 4/js/script.js
@@ -0,0 +1,7 @@
+function Calcular() {
+    let a;
+    let b;
+    a = parseInt=(prompt ("Introduce el primer número:"));
+    b = parseInt=(prompt ("Introduce el segundo número:"));
+    alert("El primer número es: " + a + " \nEl segundo número es: " + b + " \nLa suma es: " + (a+b) + "\nLa resta es: " + (a - b) +"\nEl producto es: " + (a*b) + "\nLa división: " + (a / b) + "\nEl resto es: " + (a % b) + "");
+}
